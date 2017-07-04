883ecaa5824cc4edbd97c371f29eba957fe39e6e
Mary Rose Cook <mary@maryrosecook.com>
2017-07-04 09:25:45 +0100

    Fix bug where mouse position isn't extracted when x is 0

--- App/src/coquette.js
+++ App/src/coquette.js
@@ -580,9 +580,9 @@ MouseMoveListener.prototype = {
   },
 
   _getAbsoluteMousePosition: function(e) {
-	  if (e.pageX) 	{
+	  if (e.pageX !== undefined) 	{
       return { x: e.pageX, y: e.pageY };
-	  } else if (e.clientX) {
+	  } else if (e.clientX !== undefined) {
       return { x: e.clientX, y: e.clientY };
     }
   }
