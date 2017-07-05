f06c90cf8fe151a6be46a4da1fa923771d4a7ac0
Mary Rose Cook <mary@maryrosecook.com>
2017-07-05 16:34:44 +0100

    Move enemy right

--- App/src/enemy.js
+++ App/src/enemy.js
@@ -2,10 +2,14 @@ class Enemy {
   constructor(game, settings) {
     this.center = settings.center;
     this.grid = settings.grid;
+    this.lastMoved = Date.now();
   }
 
   update() {
-
+    if (this.lastMoved + 1000 < Date.now()) {
+      this.lastMoved = Date.now();
+      this.center.x += this.grid.squareSize.x;
+    }
   }
 
   draw(screen) {
